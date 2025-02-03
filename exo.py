2a3bde0241edf66c7961ab909196de903edcdeb8
Christophe Schulz <christopheschulz@orange.fr>
2025-02-03 19:51:14 +0100

    meilleur

--- exo.py
+++ exo.py
@@ -2,12 +2,11 @@ import sys
 
 arguments = sys.argv
 
+if len(arguments) == 3 and int(arguments[2]) != 0 and int(arguments[1]) // int(arguments[2]) != 0:
+        print(f"résultat : {int(arguments[1]) // int(arguments[2])}")
+        print(f"reste: {int(arguments[1]) % int(arguments[2])}")
+        exit()
 
-if not len(arguments) == 3 or int(arguments[2]) == 0 or int(arguments[1]) // int(arguments[2]) == 0:
-    print("erreur.")
-    exit()
-
-else:
-    print(f"résultat : {int(arguments[1]) // int(arguments[2])}")
-    print(f"reste: {int(arguments[1]) % int(arguments[2])}")
+print("erreur.")
+        
 
